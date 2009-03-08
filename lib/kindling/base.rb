5d7674b4ef2574c69c9e470561f9f143be03c4cf
Jonathan S. Katz <jonathan.katz@excoventures.com>
2009-03-08 16:02:35 -0400

    Basic connection class created

---
+++ lib/kindling/base.rb
@@ -0,0 +1,73 @@
+require 'rubygems'
+require 'uri'
+require 'net/http'
+require 'net/https'
+require 'nokogiri'
+
+module Kindling
+
+  class Base
+
+    DEFAULT_CONNECTION_OPTIONS = {
+      :domain => '.campfirenow.com',
+      :port => 80,
+      :ssl => false
+    }
+
+    class << self
+
+      def connection
+        @connection
+      end
+
+      # configures the connection for the Campfire lobby we are connecting to
+      #
+      # * +lobby_name+: the name of the Campfire lobby we want to connect to
+      # * +options:+
+      #   * +:ssl+: true if we are connection over SSL.  Defaults to false
+      def initialize_connection(lobby_name, options={})
+        options = DEFAULT_CONNECTION_OPTIONS.merge(options)
+        url = URI.parse(uri_for(lobby_name, options))
+        @connection = {
+          :host => url.host,
+          :port => url.port,
+          :ssl => ssl?(url.scheme)
+        }
+      end
+
+
+      # configures the connection for the Campfire lobby we are connecting to from
+      # a Yaml configuration file
+      def initialize_connection_from_file(file_path)
+        raise Errno::ENOENT unless File.exists?(file_path)
+        file = File.open(file_path)
+        config = symbolize_keys(YAML::load(file.read))
+        p config.inspect
+        self.initialize_connection(config[:lobby_name], config[:options])
+      end
+
+    private
+
+      def ssl?(scheme)
+        scheme == 'https'
+      end
+
+      def symbolize_keys(hash)
+        return hash unless hash.kind_of?(Hash)
+        symbol_hash = {}
+        hash.each { |k,v| symbol_hash[k.to_sym] = symbolize_keys(v) }
+        symbol_hash
+      end
+
+      def uri_for(lobby_name, options)
+        scheme = ssl?(options[:ssl]) ? 'https' : 'http'
+        domain = lobby_name + options[:domain]
+        port = options[:port]
+        [scheme, '://', domain, ':', port].join
+      end
+
+    end
+
+  end
+
+end
